f7394a82f5b79392a5a1c10145d3c6a2a11c3083
wcheng47@gatech.edu <Wilche3@gmail.com>
2018-01-18 14:15:10 -0500

    added java code from recitation

---
+++ rec2/ScanTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ScanTest {
+	public static void main(String[] args) {
+		Scanner newScan = new Scanner(System.in);
+
+		System.out.println("Enter input here:");
+		String firstWord = newScan.next();
+		System.out.println(firstWord);
+
+		System.out.println("Enter input here:");
+		String firstLine = newScan.nextLine();
+		System.out.println(firstLine);
+
+		//System.out.println("Enter input here:");
+		//int firstInt = newScan.nextInt();
+		//System.out.println(firstInt);
+
+		//System.out.println("Enter input here:");
+		//Boolean hasInt = newScan.hasNextInt();
+		//System.out.println(hasInt);
+	}
+}
+
+//Using hasNext() to avoid exceptions
+//Newline character
+//
---
+++ rec2/StringTest.java
@@ -0,0 +1,30 @@
+public class StringTest {
+	public static void main(String[] args) {
+		//Following lines are equivalent:
+		String oneString = "to rule them all";
+		String twoString = new String("to rule them all");
+		//System.out.println("oneString: " + oneString);
+
+		//Following lines are equivalent:
+		String threeString = "";
+		String fourString = new String();
+
+		//strings are immutable!!!!
+		//Concatenation: merging strings
+		String fiveString = oneString + twoString;
+		System.out.println(fiveString + "\n");
+
+		//charAt: get character at an index
+		char newChar = oneString.charAt(3);
+		System.out.println("newChar: " + newChar + "\n");
+
+		//equals: boolean declaring whether two strings are equal
+		boolean areEqual = oneString.equals(twoString);
+		System.out.println("areEqual: " + areEqual + "\n");
+
+		//length: pretty self explanatory
+		System.out.println(oneString.length());
+
+		//CHECK THE API!!!!!!!!!!!
+	}
+}
